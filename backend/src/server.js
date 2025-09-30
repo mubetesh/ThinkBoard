b4b6d77f88f3f6242b0ee4f40063ed1ba6656ed1
mubetesh <mubeteshe55@gmail.com>
2025-09-30 12:37:48 +0300

    add production on server

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,16 +5,28 @@ import connectDB from './config/db.js';
 import dotenv from 'dotenv';
 import rateLimiter from './middleware/ratelimiter.js';
 import cors from "cors"
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // Middleware
-app.use(cors({origin : "http://localhost:5173" })) // Adjust the origin as per your frontend's address
+if (process.env.NODE_ENV !== 'production') {
+  app.use(cors({ origin: "http://localhost:5173" }));
+}
 app.use(express.json()); // used to parse JSON request bodies and populate req.body
 app.use(rateLimiter); // Apply rate limiting middleware to all routes
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve static files from the React app
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+}
+ app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});
 
 // Connect to Database
 connectDB();
